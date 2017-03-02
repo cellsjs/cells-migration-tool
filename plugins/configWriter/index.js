8c372ab2c4845251516c0369eca751d6fbef6dbe
sbonacho <sbonacho@gmail.com>
2017-03-02 12:19:24 +0100

    feat(): append config files if exists : .gradle/gradle.properties and .netrc with environment:setup

--- plugins/configWriter/index.js
+++ plugins/configWriter/index.js
@@ -16,9 +16,19 @@ module.exports = {
     _writeFiles() {
       if (this.params.configFiles) {
         Object.getOwnPropertyNames(this.params.configFiles).forEach((file) => {
-          let content;
-          eval(`content = \`${this.params.configFiles[file]}\``, this.params);
-          fs.writeFileSync(path.join(process.env.HOME, file), content);
+          let content = '';
+          if (this.fsExists(file)) {
+            content = this.fsReadFile(file)+ '\n';
+            if (content.indexOf(this.params.registryDomain) >= 0) {
+              this.logger.info('file', '#cyan', file, 'is already configured!');
+              content = undefined;
+            }
+          }
+          if (content !== undefined) {
+            eval(`content += \`${this.params.configFiles[file]}\``, this.params);
+            fs.writeFileSync(path.join(process.env.HOME, file), content);
+            this.logger.info('file', '#green', file, 'written');
+          }
         });
       }
     }
