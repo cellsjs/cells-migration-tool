ba966d8839e954665f4fbe05c10bc6b398880fcc
sbonacho <sbonacho@gmail.com>
2017-03-02 15:51:40 +0100

    feat(): files was overwritten all the time.

--- plugins/configWriter/index.js
+++ plugins/configWriter/index.js
@@ -16,18 +16,14 @@ module.exports = {
     _writeFiles() {
       if (this.params.configFiles) {
         Object.getOwnPropertyNames(this.params.configFiles).forEach((file) => {
-          let content = '';
-          if (this.fsExists(file)) {
-            content = this.fsReadFile(file)+ '\n';
-            if (content.indexOf(this.params.registryDomain) >= 0) {
-              this.logger.info('file', '#cyan', file, 'is already configured!');
-              content = undefined;
-            }
-          }
-          if (content !== undefined) {
+          const filePath = path.join(process.env.HOME, file);
+          let content = this.fsReadFile(filePath);
+          if (content.indexOf(this.params.registryDomain) >= 0) {
+            this.logger.info('file', '#cyan', filePath, 'is already configured!');
+          } else {
             eval(`content += \`${this.params.configFiles[file]}\``, this.params);
-            fs.writeFileSync(path.join(process.env.HOME, file), content);
-            this.logger.info('file', '#green', file, 'written');
+            fs.writeFileSync(filePath, content);
+            this.logger.info('file', '#green', filePath, 'written');
           }
         });
       }
