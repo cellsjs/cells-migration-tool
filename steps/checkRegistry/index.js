42ed7e3f92944ec82989cfe1d36012d318694b29
Felipe Roucher <felipe.roucher@bbva.com>
2017-03-13 12:13:19 +0100

    feat(*): improving user experience
    - email filter
    - order asking first API Key
    
    [DEP-80]

--- steps/checkRegistry/index.js
+++ steps/checkRegistry/index.js
@@ -1,4 +1,7 @@
 module.exports = {
+  checkEmail(email) {
+    return email.replace(/@.*/, '');
+  },
   check() {
     this.logger.info('registry:', '#cyan', this.params.registryDomain, 'is configured', '#green', 'OK');
   }
